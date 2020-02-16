63896b4443fc353491101d4c56282452f64ca2c0
Dart513 <30852789+Dart513@users.noreply.github.com>
2020-02-15 22:12:24 -0500

    Added new functions

--- src/main/java/viking/math/General.java
+++ src/main/java/viking/math/General.java
@@ -8,5 +8,11 @@ public class General {
         public double findx(double theta, double hyp) {return hyp*(Math.sin(Math.toRadians(theta)));}
         public double findy(double theta, double hyp) {return hyp*(Math.cos(Math.toRadians(theta)));}
 
-        public double hyplength(double x, double y) {return Math.sqrt(x*x+y*y);}
+        public double hypLength(double x, double y) {return Math.sqrt(x*x+y*y);}
+
+        public double distToRotations(double radius, double distance){return distance/(2*pi*radius);}
+        public double distToTicks(double radius, double distance, double ticksPerRotation){return (distance/(2*pi*radius))*ticksPerRotation;}
+        public double velocity(double d1, double d2, double time){return (d2-d1)/time;}
+        public double acceleration(double v1, double v2, double time){return v2-v1/time;}
+        public double acceleration(double d1, double d2, double d3, double time){return ((d3-d2)-(d2-d1))/time;}
 }
