d2c65c1b97626c2a6492a766ce4113ba11e8393a
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2021-10-30 21:00:03 -0400

    Implement logging functionality

---
+++ src/main/java/viking/Logging.java
@@ -0,0 +1,32 @@
+package viking;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class Logging {
+	/**
+	 * Log string with timestamp
+	 * @param str String to log
+	 */
+	public static void log(String str) {
+		System.out.printf("[%.5f] %s\n", Timer.getFPGATimestamp(), str);
+	}
+
+	/**
+	 * Log current function call
+	 */
+	public static void trace() {
+		StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+		System.out.printf("[%.5f] TRACE %s:%d %s.%s\n", Timer.getFPGATimestamp(),
+						  stacktrace[2].getFileName(), stacktrace[2].getLineNumber(),
+						  stacktrace[2].getClassName(), stacktrace[2].getMethodName());
+	}
+
+	/**
+	 * Log current function call as unimplemented
+	 */
+	public static void unimp() {
+		StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+		System.out.printf("[%.5f] UNIMPLEMENTED %s.%s\n", Timer.getFPGATimestamp(),
+						  stacktrace[2].getClassName(), stacktrace[2].getMethodName());
+	}
+}
