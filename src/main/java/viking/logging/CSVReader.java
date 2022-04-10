6154bf9daa2bc79d0647a8d464be5eed487d635d
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2022-04-10 18:29:31 -0400

    Initial Commit

---
+++ src/main/java/viking/logging/CSVReader.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class CSVReader {
+
+    public static ArrayList<ArrayList<Double>> Reader (String filename) {
+
+        String line; // Empty String variable to read the CSV values to
+        ArrayList<ArrayList<Double>> positionsArrayList = new ArrayList<>(); // ArrayList where positions coordinates are stored
+
+        try {
+            // Starts bufferedReader
+            BufferedReader br = new BufferedReader(new FileReader(filename)); 
+
+            // Read CSV top to bottom until there is a line that contains nothing
+            while ((line = br.readLine()) != null) { 
+                String[] values = line.split(",");
+                
+                // Converts string array into double array
+                ArrayList<Double> coordinates = new ArrayList<Double>();
+
+                // Iterates through String Array and converts position elements into doubles
+                for (int i = 0; i < values.length; i++) {
+                    coordinates.add(Double.parseDouble(values[i]));
+                }
+                
+                // Nests coordinates arrayList into the main arrayList
+                positionsArrayList.add(coordinates); 
+
+            }
+
+            // Closes bufferedReader instance
+            br.close();
+
+        }
+        
+        // Exceptions
+        catch (FileNotFoundException e) {
+            System.out.println("[CSVReader] The CSV file could not be found.");
+            e.printStackTrace();
+        }
+
+        catch (IOException e) {
+            System.out.println("[CSVReader] IO error.");
+            e.printStackTrace();
+        }
+
+        return positionsArrayList;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
