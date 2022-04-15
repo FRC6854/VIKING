dbe1eb08c0a8a7747238a9110fdc26173168a705
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2022-04-15 13:09:50 -0400

    Add package declaration and clean up imports for CSV reader and writer

--- src/main/java/viking/logging/CSVReader.java
+++ src/main/java/viking/logging/CSVReader.java
@@ -1,5 +1,10 @@
-import java.io.*;
-import java.util.*;
+package viking.logging;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class CSVReader {
 
