0cb84b5758295de744cbc8b7a4f4b4aa0a6045c8
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2022-06-19 15:56:16 -0400

    Initial commit for gearConversion
    - Calculates motor rotation
    - Calculates wheel rotation

---
+++ src/main/java/viking/math/gearConversion.java
@@ -0,0 +1,23 @@
+package viking.math;
+
+public class gearConversion {
+    /**
+     * Calculates motor rotation given wheel rotation and gear ratio 
+     * @param wheelRotation
+     * @param ratio
+     * @return
+     */
+    public static double getMotorRotation(double wheelRotation, double ratio) {
+        return wheelRotation * ratio;
+    }
+
+    /**
+     * Calculates wheel rotation given motor rotation and gear ratio 
+     * @param motorRotation
+     * @param ratio
+     * @return
+     */
+    public static double getWheelRotation(double motorRotation, double ratio) {
+        return motorRotation / ratio;
+    } 
+}
